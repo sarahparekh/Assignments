135d17c2fa2883a42fec3c292f9e8000e8f072f9
Sarah <106901182+sarahparekh@users.noreply.github.com>
2026-04-20 07:52:30 +0530

    Create Assignment13.c

---
+++ Assignment13.c
@@ -0,0 +1,16 @@
+// Reverse Digits
+#include <stdio.h>
+
+int main(){
+    int n,r;
+
+    scanf("%d",&n);
+
+    while(n!=0){
+        r=n%10;
+        printf("%d ",r);
+        n/=10;
+    }
+
+    return 0;
+}
